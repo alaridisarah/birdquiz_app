3d8071c5879f69bf860b67146865c18ad51a08bd
alaridisarah <92929752+alaridisarah@users.noreply.github.com>
2024-03-30 21:21:04 +0300

    trying to add notification feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:quiz_app/screens/login_screen.dart';
 import 'package:quiz_app/screens/quiz_screen.dart';
 import 'package:quiz_app/screens/score_screen.dart';
 import 'package:quiz_app/screens/splash_screen.dart';
 import 'package:quiz_app/screens/category_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+Future<void> main() async {
   // the enrty point in our project
   runApp(const MyApp());
+// the code below doesn't work
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCMToken $fcmToken");
 }
 
+/// end of code
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
